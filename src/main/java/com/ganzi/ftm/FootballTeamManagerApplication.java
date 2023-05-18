83c96dfc589bb01841d35c52bbbe20811df36048
ganzi <ganzi324@gmail.com>
2023-05-19 00:09:54 +0900

    First commit

---
+++ src/main/java/com/ganzi/ftm/FootballTeamManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ganzi.ftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganzi/ftm/FootballTeamManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzi.ftm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
